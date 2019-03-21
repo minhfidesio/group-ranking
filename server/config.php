033451d266a7caa83f5c287fe4a6a7a269e795a1
minhnguyen_fidesio <minh.nguyen@fidesio.com>
2019-03-21 16:19:54 +0700

    first commit

---
+++ server/config.php
@@ -0,0 +1,24 @@
+<?php 
+/* Developed by Vy Nghia */
+error_reporting(0); // Turn off error reporting text
+
+// Since last [rank_day] days
+define('day_rank', 30);
+
+// Secret key
+define('SECRET', 'NghiaisGay');
+
+// Group ID
+define('GroupID', '000000000000000');
+
+// Your Token (Please using iPhone/Android/iOS access_token)
+define('access_token', '275058599931625');
+
+// Arrray Points Count
+$pnt = array(
+	'points_per_post' => 5,
+	'points_per_comment' => 2,
+	'points_per_commented' => 2,
+	'points_per_reaction' => 1,
+	'points_per_reacted' => 1
+);
